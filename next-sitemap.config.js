f630cb41512d181012906c6761b034e6f634f83e
simanto-sroy <sroy38703@gmail.com>
2023-12-06 07:51:45 +0600

    feat: -updated-seo-files

---
+++ next-sitemap.config.js
@@ -0,0 +1,35 @@
+// import('next-sitemap').IConfig
+
+module.exports = {
+    siteUrl: 'https://simanto-roy.vercel.app/',
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    generateRobotsTxt: true,
+    alternateRefs: [
+        {
+            href: 'https://sroy-simanto.github.io/',
+            hreflang: 'es',
+        },
+    ],
+    images: [{ loc: 'https://simanto-roy.vercel.app/assets/simanto-roy.jpg' }],
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: 'test-bot',
+                allow: ['/data/about.js', '/data/home.js', '/data/projects.js', '/data/services.js', '/data/testimonials.js', '/data/common/socialLinks.js', '/seo.config.js'],
+            },
+            {
+                userAgent: 'black-listed-bot',
+                disallow: ['/components', '/styles'],
+            },
+        ],
+        additionalSitemaps: [
+            'https://simanto-roy.vercel.app/my-custom-sitemap-1.xml',
+        ],
+    },
+}
